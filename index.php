b24d37ed5ce95d88bfa49fc75887dba30f7dc13c
Kane026 <frentzkane@gmail.com>
2025-05-12 08:50:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=header, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    </header>
+        <main>
+            <h1>KND AIR</h1>
+            <?php phpinfo(); ?>
+        </main>
+    </header>
+
+<footer></footer>
+</body>
+</html>
