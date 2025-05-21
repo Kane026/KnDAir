fd1cfbb8acff84300baaf3d78f96328daf45527f
Kane026 <frentzkane@gmail.com>
2025-05-21 08:48:20 +0200

    home page + header

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+
+    
+    <div class="header">
+  <img class="knd-air-logo-header" src="assets/img/knd-air-logo.png" alt="" height="200px">
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Reizen</a>
+    <a href="#about">Over ons</a>
+    <a href="#about">Contact</a>
+  </div>
+  <button onclick="location.href='index.php'" class="login">login</button>
+</div>
--- index.php
+++ index.php
@@ -1,19 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=header, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>KnDair</title>
 </head>
+</head>
+
 <body>
     <header>
+        <?php include('./includes/header.php') ?>
     </header>
-        <main>
-            <h1>KND AIR</h1>
-            <?php phpinfo(); ?>
-        </main>
+    <main>
+        <div class="banner-video">
+            <video class="header-video" autoplay muted loop playsinline>
+                <source src="assets/video/theme-park.mp4" type="video/mp4">
+            </video>
+            <div class="video-overlay-content">
+                <h1 class="video-title">KND AIR</h1>
+                <button class="fill-button">Boek hier</button>
+            </div>
+        </div>
+        <div class="populaire-bestemmingen-container">
+            <h1 class="populaire-bestemmingen-text">Populaire bestemmingen</h1>
+            <div class="pop-bes-images">
+                <img src="assets/img/tokyo.png" alt="" class="tokyo-img">
+                <img src="assets/img/florida.png" alt="" class="florida-img">
+                <img src="assets/img/parijs.png" alt="" class="parijs-img">
+                <img src="assets/img/spanje.png" alt="" class="spanje-img">
+                <img src="assets/img/duitsland.png" alt="" class="duitsland-img">
+            </div>
+        </div>
+        <h1>Zoek op accommodatietypes</h1>
+    </main>
     </header>
 
-<footer></footer>
+    <footer>
+
+    </footer>
 </body>
+
 </html>
