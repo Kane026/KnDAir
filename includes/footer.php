679483e984613726c94179c4951442dc33b6c83b
Kane026 <frentzkane@gmail.com>
2025-06-11 10:37:54 +0200

    privacy policy & footer added

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<div class="footer">
+    <div class="voorwaarden-footer">
+        <h1>Voorwaarden & Contacten</h1>
+        <p>Algemene voorwaarden<p>
+        <p>Contact</p>
+    </div>    
+    <div class="privacy-policy-footer">
+        <h1>Privacy</h1>
+        <a href="privacy-beleid.php">Privacy beleid</a>
+    </div>
+    <div class="socials-footer">
+        <h1>Socials</h1>
+        <a href="https://open.spotify.com/playlist/4vEtC3m3FcHpBbqiQsIf02?si=7af0cd37882245fc" target="_blank">Spotify</a>
+        <p>Instagram</p>
+        <p>X</p>
+    </div>
+
+</div>
